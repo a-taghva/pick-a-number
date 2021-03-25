414d292ba1bfed2584eeaa81e2ea8fba03021e1c
Ali <taghvali99@gmail.com>
2021-03-24 23:06:51 -0400

    pick only one unique number everytime

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const numArr = generateNumArr(15);
+
 function generateNumArr(n) {
 	const numArr = [];
 	let i = 0;
@@ -12,13 +14,14 @@ function pickANum(n) {
 	return Math.floor(Math.random() * n)
 };
 
-function chooseOne(n) {
-	const numArr = generateNumArr(n);
+function chooseOne() {
+	const l = numArr.length;
 
-	while (numArr.length) {
-		const l = numArr.length;
-		const num = pickANum(l);
-
-		console.log(numArr.splice(num, 1)[0]);
+	if (l) {
+		const n = pickANum(l);
+		console.log(numArr.splice(n, 1)[0]);
+	} else {
+		alert('There\'s nothing to select buddy!');
 	};
+
 };
